fac12df39b36faed887b583b2abdc9da0ca71b89
Anant Tater <77228263+ananttater@users.noreply.github.com>
2023-06-20 10:56:53 +0530

    Update script.js with New York Times API

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-// const API_KEY = "ca8c8312b9e94aad91d9a7e73b77cb68";
-const url = "https://api.worldnewsapi.com/search-news?api-key=ca8c8312b9e94aad91d9a7e73b77cb68&text=";
-
-window.addEventListener("load", ()=> fetchNews(""));
-// ()=> arrow functions do not allow duplicate parameters, 
-// whether in strict or non-strict mode.Duplicate parameters 
-// will cause a Syntax Error to be thrown. 
+const url = "https://api.nytimes.com/svc/news/v3/content/all/";
+const API_KEY = ".json?api-key=oeJRUhXapfEYkvDwSOPiOcBYDSqzdPhr";
+window.addEventListener("load", ()=> fetchNews("all"));
 
 function reload(){
     window.location.reload();
 }
 
 async function fetchNews(query) {
-    const res = await fetch(`${url}${query}&earliest-publish-date=2023-01-25`);
+    const res = await fetch(`${url}${query}${API_KEY}`);
     const data = await res.json();
-    bindData(data.news);
+    bindData(data.results);
 }
 
 function bindData(articles) {
@@ -23,19 +19,12 @@ function bindData(articles) {
     cardsContainer.innerHTML = "";
 
     articles.forEach((article) => {
-        // if (!article.image) return;
+        // if (!article.thumbnail_standard) return;
         const cardClone = newsCardTemplate.content.cloneNode(true);
         fillDataInCard(cardClone, article);
         cardsContainer.appendChild(cardClone);
     });
 }
-
-// The cloneNode() method of the Node interface returns a duplicate of 
-// the node on which this method was called. Its parameter controls if 
-// the subtree contained in a node is also cloned or not.
-
-// The appendChild() method of the Node interface adds a node to the 
-// end of the list of children of a specified parent node.
 
 function fillDataInCard(cardClone, article){
     const newsImg = cardClone.querySelector('#news-img');
@@ -43,22 +32,15 @@ function fillDataInCard(cardClone, article){
     const newsSource = cardClone.querySelector('#news-source');
     const newsDesc = cardClone.querySelector('#news-desc');
     
-    if(!article.image) newsImg.src = "https://via.placeholder.com/400x200";
-    else newsImg.src = article.image;
+    newsImg.src = article.multimedia[2].url;
     
     newsTitle.innerHTML = article.title;
-    newsDesc.innerHTML = article.text;
+    newsDesc.innerHTML = article.abstract;
 
-    // const date = new Date(article.publishedAt).toLocaleString("en-US", {
-    //     timeZone: "Asia/Jakarta"
-    // })
-
-    // The toLocaleString() method returns a string with a 
-    // language-sensitive representation of this date. In 
-    // implementations with Intl.DateTimeFormat API support, 
-    // this method simply calls Intl.DateTimeFormat.
-
-    newsSource.innerHTML = `${article.author} • ${article.publish_date}`;
+    const date = new Date(article.published_date).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    })
+    newsSource.innerHTML = `${article.byline} • ${date}`;
 
     cardClone.firstElementChild.addEventListener("click", () => {
         window.open(article.url, "_blank");
